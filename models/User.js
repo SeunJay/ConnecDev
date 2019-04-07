25184c4401ce349e19719b5b2f3a3338adb41299
seun jay <seunjay92@gmail.com>
2019-04-07 15:35:38 +0100

    create model and schema for user

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose'); 
+const Schema = mongoose.Schema;
+
+//create schema
+ const userSchema = new Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    avatar: {
+      type: String,
+      required: true
+    },
+    Date: {
+      type: Date,
+      default: Date.now
+    },
+ })
+
+ module.exports = User = mongoose.model('users', userSchema);
