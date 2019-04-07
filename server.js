5becd967245e8110699d2f9f1886b32322de7228
seun jay <seunjay92@gmail.com>
2019-04-07 15:08:39 +0100

    Initial express server with routes file

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.send('Hello World my main Geeesss'));
 
 // Use Routes
 app.use('/api/users', users);
